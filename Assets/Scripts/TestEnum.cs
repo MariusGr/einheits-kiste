ee079671969e6fb57e88c046ef6ff04d064598a1
Marius Gripp <marius.gripp@arcor.de>
2024-05-21 15:28:35 +0200

    Setup Unity project

---
+++ Assets/Scripts/TestEnum.cs
@@ -0,0 +1,12 @@
+namespace KeyObjects
+{
+
+    public enum TestEnum
+    {
+        None = 0,
+        Etwas = 1,
+        NochEtwas = 2,
+        GanzWasAnderes = 3,
+        WOOOOOW = 4,
+    }
+}
---
+++ Assets/Scripts/TestEnum2.cs
@@ -0,0 +1,10 @@
+namespace KeyObjects
+{
+    public enum TestEnum2
+    {
+        None = 0,
+        Etwas = 1,
+        NochEtwas = 2,
+        GanzWasAnderes = 3,
+    }
+}
