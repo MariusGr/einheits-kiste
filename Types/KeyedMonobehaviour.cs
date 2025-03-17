f8f5c3344b24e25abe413824fe17f4c3ab8306bc
MariusGr <marius.gripp@arcor.de>
2025-03-17 17:57:33 +0100

    Implement KeyedMonoBehaviours

---
+++ Types/KeyedMonobehaviour.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using MyBox;
+using UnityEngine;
+
+namespace EinheitsKiste
+{
+    // TODO Get working with non-ExecuteInEditMode MonoBehaviours
+    public abstract class KeyedMonobehaviour<T> : MonoBehaviour where T : KeyedMonobehaviour<T>
+    {
+        public abstract string Key { get; }
+
+        private static readonly Dictionary<string, T> _instances = new();
+        private static T[] GetInstances() => FindObjectsByType<T>(FindObjectsSortMode.None);
+
+        public static T Get(string key)
+        {
+            print("get " + key);
+            if (_instances.TryGetValue(key, out T instance))
+            {
+                print(instance);
+                return instance;
+            }
+
+            instance = GetInstances().Where(i => i.Key == key).FirstOrDefault();
+
+            if (instance != null)
+            {
+                _instances.Add(key, instance);
+                return instance;
+            }
+
+            Debug.LogWarning($"No instance of {typeof(T).Name} found with key {key}");
+            return null;
+        }
+
+        protected virtual void OnValidate()
+        {
+            if (Key.IsNullOrEmpty()) return;
+            if (_instances.ContainsKey(Key)) return;
+
+            print("added " + Key);
+            _instances.Add(Key, (T)this);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (Key.IsNullOrEmpty()) return;
+            if (!_instances.ContainsKey(Key)) return;
+            print("removed " + Key);
+
+            _instances.Remove(Key);
+        }
+    }
+}
