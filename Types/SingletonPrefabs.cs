a59f668378ee856056691e5c472ce15851fbcb26
MariusGr <marius.gripp@arcor.de>
2026-01-28 15:35:38 +0100

    Fix: Too many types displayed for key selection

--- Types/SingletonPrefabs.cs
+++ Types/SingletonPrefabs.cs
@@ -4,13 +4,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using SolidUtilities.Collections;
 using TypeReferences;
+using System.Runtime.InteropServices;
 
 namespace EinheitsKiste
 {
     [CreateAssetMenu(fileName = "SingletonPrefabs", menuName = "EinheitsKiste/Singleton Prefabs")]
     public class SingletonPrefabs : ScriptableObjectSingleton<SingletonPrefabs>
     {
-        [SerializeField] private SerializableDictionary<TypeReference, GameObject> _prefabSingletons = new();
+        [Serializable]
+        public struct SingletonPrefab
+        {
+            [field: SerializeField, Inherits(typeof(MonoBehaviour))] public TypeReference Type { get; private set; }
+            [field: SerializeField] public GameObject Prefab { get; private set; }
+
+            public override readonly bool Equals(object obj)
+            {
+                if (obj is SingletonPrefab other) return Type.Type == other.Type.Type && Prefab == other.Prefab;
+                return false;
+            }
+
+            public override readonly int GetHashCode() => HashCode.Combine(Type.Type, Prefab);
+        }
+
+        [SerializeField] private SingletonPrefab[] _prefabSingletons;
 
         private Dictionary<Type, GameObject> _prefabSingletonsCache;
         public IReadOnlyDictionary<Type, GameObject> PrefabSingletons
@@ -20,8 +36,8 @@ namespace EinheitsKiste
                 if (_prefabSingletonsCache == null)
                 {
                     _prefabSingletonsCache = _prefabSingletons.ToDictionary(
-                        kvp => kvp.Key.Type,
-                        kvp => kvp.Value);
+                        prefab => prefab.Type.Type,
+                        prefab => prefab.Prefab);
                 }
                 return _prefabSingletonsCache;
             }
@@ -29,14 +45,24 @@ namespace EinheitsKiste
 
         private void OnValidate()
         {
-            var duplicateKeys = _prefabSingletons.Keys
-                .GroupBy(k => k.Type)
-                .Where(g => g.Count() > 1)
+            var duplicateKeys = _prefabSingletons
+                .GroupBy(prefab => prefab.Type.Type)
+                .Where(group => group.Count() > 1)
                 .ToList();
 
             foreach (var group in duplicateKeys)
             {
                 Debug.LogError($"Duplicate singleton prefab type found: {group.Key}", this);
+            }
+
+            var duplicateValues = _prefabSingletons
+                .GroupBy(prefab => prefab.Prefab)
+                .Where(group => group.Count() > 1)
+                .ToList();
+
+            foreach (var group in duplicateValues)
+            {
+                Debug.LogError($"Duplicate singleton prefab found: {group.Key}", this);
             }
         }
     }
