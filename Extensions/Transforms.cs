fb14029d16bb5392800d8cbb168859120294b57d
MariusGr <marius.gripp@arcor.de>
2026-06-10 13:33:37 +0200

    Cleanup

--- Extensions/Transforms.cs
+++ Extensions/Transforms.cs
@@ -5,9 +5,6 @@ namespace EinheitsKiste
     public static class Transforms
     {
         public static void RotateAround(Transform transform, Vector3 pivotPoint, Quaternion rot)
-        {
-            transform.position = rot * (transform.position - pivotPoint) + pivotPoint;
-            transform.rotation = rot * transform.rotation;
-        }
+            => transform.SetPositionAndRotation(rot * (transform.position - pivotPoint) + pivotPoint, rot * transform.rotation);
     }
 }
